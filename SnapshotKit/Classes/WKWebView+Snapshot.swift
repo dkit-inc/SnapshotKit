31ac3a633919408709f2f5eb71f756553f38e770
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2022-06-17 03:53:00 +0900

    add maxpage

--- SnapshotKit/Classes/WKWebView+Snapshot.swift
+++ SnapshotKit/Classes/WKWebView+Snapshot.swift
@@ -49,6 +49,30 @@ extension WKWebView {
         })
     }
 
+    override
+    public func asyncTakeSnapshotOfFullContent(maxPage: Int = 10, _ completion: @escaping ((UIImage?) -> Void)) {
+        let originalOffset = self.scrollView.contentOffset
+
+        // 当contentSize.height<bounds.height时，保证至少有1页的内容绘制
+        var pageNum = 1
+        if self.scrollView.contentSize.height > self.scrollView.bounds.height {
+            pageNum = Int(floorf(Float(self.scrollView.contentSize.height / self.scrollView.bounds.height)))
+        }
+        if pageNum > maxPage {
+            pageNum = maxPage
+        }
+
+        self.loadPageContent(0, maxIndex: pageNum, completion: {
+            self.scrollView.contentOffset = CGPoint.zero
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                let renderer = WebViewPrintPageRenderer.init(formatter: self.viewPrintFormatter(), contentSize: self.scrollView.contentSize)
+                let image = renderer.printContentToImage()
+                self.scrollView.contentOffset = originalOffset
+                completion(image)
+            }
+        })
+    }
+
     fileprivate func loadPageContent(_ index: Int, maxIndex: Int, completion: @escaping () -> Void) {
         self.scrollView.setContentOffset(CGPoint(x: 0, y: CGFloat(index) * self.scrollView.frame.size.height), animated: false)
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
